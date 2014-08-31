b9f4d5ba33b1107b0a429860de118776b8866267
Nedyalko Andreev <nedyalko.andreev@gmail.com>
2014-08-31 19:15:35 +0300

    More experimentation

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
+	"sync"
 	"time"
+
+	log "github.com/golang/glog"
 
 	"github.com/nictuku/dht"
 )
@@ -15,45 +17,81 @@ func main() {
 	// To see logs, use the -logtostderr flag and change the verbosity with
 	// -v 0 (less verbose) up to -v 5 (more verbose).
 	if len(flag.Args()) != 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %v <infohash>\n\n", os.Args[0])
+		log.Errorf("Usage: %v <infohash>\n\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	// Parses the infohash from the commandline
-	infohash, err := dht.DecodeInfoHash(flag.Args()[0])
+	infoHash, err := dht.DecodeInfoHash(flag.Args()[0])
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "DecodeInfoHash error: %v\n", err)
+		log.Errorf("WINSTON: DecodeInfoHash error: %v\n", err)
 		os.Exit(1)
 	}
 
 	// Starts a DHT node with the default options. It picks a random UDP port. To change this, see dht.NewConfig.
 	d, err := dht.New(nil)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "New DHT error: %v", err)
+		log.Errorf("WINSTON: New DHT error: %v\n", err)
 		os.Exit(1)
 	}
 
 	go d.Run()
-	go drainresults(d)
+
+	var wg sync.WaitGroup
+
+	//TODO: run this for every unresolved infohash (with throtling of course)
+	wg.Add(1)
+	go downloadTorrent(d, &wg, infoHash)
+	wg.Wait()
+}
+
+func downloadTorrent(d *dht.DHT, wg *sync.WaitGroup, infoHash dht.InfoHash) {
+	defer wg.Done()
+
+	// This channel will be used to signal when the file is downloaded
+	torrentDownloaded := make(chan bool)
+
+	// Launch the gorouitine that will contact peers and try to download the file
+	go connectToPeers(d, infoHash, torrentDownloaded)
+
+	tick := time.Tick(10 * time.Second)
+
 	for {
-		d.PeersRequest(string(infohash), false)
-		time.Sleep(15 * time.Second)
+		select {
+		case <-torrentDownloaded:
+			log.V(1).Infof("WINSTON: Torrent '%x' was successfully downloaded!!!\n", infoHash)
+			return
+
+		case <-time.After(30 * time.Second):
+			log.V(1).Infof("WINSTON: Could not download torrent '%x': timed out\n", infoHash)
+			os.Exit(0)
+			return
+
+		case <-tick:
+			// Repeat the request until a result appears, querying nodes that haven't been
+			// consulted before and finding close-by candidates for the infohash.
+			d.PeersRequest(string(infoHash), false)
+		}
 	}
 }
 
 // drainresults loops, printing the address of nodes it has found.
-func drainresults(n *dht.DHT) {
-	count := 0
+func connectToPeers(n *dht.DHT, infoHash dht.InfoHash, torrentDownloaded chan bool) {
+	//count := 0
 	for r := range n.PeersRequestResults {
-		for _, peers := range r {
-			for _, x := range peers {
-				fmt.Printf("########## PEER %d ##########: %v\n", count, dht.DecodePeerAddress(x))
-				count++
-				if count >= 5 {
-					os.Exit(0)
+		for foundHash, foundPeers := range r {
+			log.V(1).Infof("WINSTON: FOUND PEERS FOR '%x': %#v\n", foundHash, foundPeers)
+			torrentDownloaded <- true
+			/*
+				for _, x := range peers {
+					fmt.Printf("########## PEER %d ##########: %v\n", count, dht.DecodePeerAddress(x))
+					count++
+					if count >= 5 {
+						os.Exit(0)
+					}
 				}
-			}
+			*/
 		}
 	}
 }
